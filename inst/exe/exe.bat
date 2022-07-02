2cf2dccb6c1729d7492a20ca54cf41397ec7b06f
CIMES-USNA-UMA <unidad.sna.cimes@fguma.es>
2022-07-02 05:03:18 +0200

    Added files to call app for testing

---
+++ inst/exe/exe.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files\R\R-4.1.1\bin\Rscript.exe" -e "shiny::runApp('C:/Users/ServNFS/Documents/R/win-library/4.1/CardioRVARapp/app/app.R', launch.browser = TRUE)"
---
+++ inst/exe/exe.vbs
@@ -0,0 +1,4 @@
+
+
+Set objShell = WScript.CreateObject("WScript.Shell")
+objShell.Run("C:/Users/ServNFS/Documents/R/win-library/4.1/CardioRVARapp/exe/exe.bat"), 0, True
