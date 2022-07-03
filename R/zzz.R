88b83f7bb013a20793deae2928c7d9b78142d3eb
CIMES-USNA-UMA <unidad.sna.cimes@fguma.es>
2022-07-03 15:23:48 +0200

    Fixed zzz.R (5)

--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,7 @@
     # Update batch file
     pckLocation <- find.package("CardioRVARapp")
       # To prevent 00LOCK:
-    pckLocation2 <- gsub("00LOCK-CardioRVARapp/00new/", "", pckLocation,
+    pckLocation <- gsub("00LOCK-CardioRVARapp/00new/", "", pckLocation,
                         fixed  = TRUE)
     shortcLoc <-  paste(pckLocation, "/CardioRVAR.lnk", sep = "")
     batch <- paste(pckLocation, "/exe/exe.bat", sep = "")
@@ -19,22 +19,20 @@
     writeLines(new_code, con = batch)
     # Update vbScript for batch file
     exevbs <- paste(pckLocation, "/exe/exe.vbs", sep = "")
-    exevbs2 <- paste(pckLocation2, "/exe/exe.vbs", sep = "")
     vbscode = readLines(exevbs)
-    vbscode = gsub("%CarioRVAR_app_directory_folder%", paste(pckLocation2, "/exe",
+    vbscode = gsub("%CarioRVAR_app_directory_folder%", paste(pckLocation, "/exe",
                                                            sep = ""), vbscode,
                    fixed = TRUE)
     writeLines(vbscode, exevbs)
     # Update shortcut generator
     shcgen <- paste(pckLocation, "/shc/shc.vbs", sep = "")
     icon <- paste(pckLocation, "/shc/icon.ico", sep = "")
-    icon2 <- paste(pckLocation2, "/shc/icon.ico", sep = "")
     shcgen_code <- readLines(shcgen)
     new_shcgen_code = gsub("%CardioRVAR_app_shortcut%", shortcLoc
                            , shcgen_code, fixed = TRUE)
-    new_shcgen_code = gsub("%CardioRVAR_app_vbScript_for_shortcut%", exevbs2
+    new_shcgen_code = gsub("%CardioRVAR_app_vbScript_for_shortcut%", exevbs
                            , new_shcgen_code, fixed = TRUE)
-    new_shcgen_code = gsub("%CardioRVAR_app_icon_for_shortcut%", icon2
+    new_shcgen_code = gsub("%CardioRVAR_app_icon_for_shortcut%", icon
                            , new_shcgen_code, fixed = TRUE)
     writeLines(new_shcgen_code, shcgen)
     # Create Shortcut
